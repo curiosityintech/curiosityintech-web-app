3e6be746c94e934c22a7d7de4c3c2c4e3cec0d53
adityak16 <adityak@geekyants.com>
2023-08-14 04:10:43 +0530

    changes in UI

--- components/FeedbackCard.tsx
+++ components/FeedbackCard.tsx
@@ -5,51 +5,49 @@ import { FeedbackType } from '../types/sections';
 
 const FeedbackCard = ({ name, profilePic }: FeedbackType) => {
   return (
-    <Fade bottom duration={500}>
-      <div className="d-flex px-3">
+    <div className="d-flex px-3">
+      <div
+        style={{
+          flexDirection: 'column',
+          justifyContent: 'center',
+          display: 'flex',
+        }}
+      >
         <div
           style={{
-            flexDirection: 'column',
-            justifyContent: 'center',
+            backgroundColor: 'rgba(89, 180, 199, 0.8)',
+            width: '12rem',
+            height: '12rem',
+            borderRadius: '50%',
             display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginBottom: '10px',
           }}
         >
-          <div
+          <img
+            src={profilePic}
             style={{
-              backgroundColor: 'rgba(89, 180, 199, 0.8)',
-              width: '12rem',
-              height: '12rem',
+              width: '10rem',
+              height: '10rem',
               borderRadius: '50%',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              marginBottom: '10px',
             }}
-          >
-            <img
-              src={profilePic}
-              style={{
-                width: '10rem',
-                height: '10rem',
-                borderRadius: '50%',
-              }}
-              className="mb-3"
-              alt={profilePic}
-            />
-          </div>
-          <h5
-            style={{ color: '#fff', justifyContent: 'center', display: 'flex' }}
-          >
-            {name}
-          </h5>
-          <h5
-            style={{ color: '#fff', justifyContent: 'center', display: 'flex' }}
-          >
-            Co-founder
-          </h5>
+            className="mb-3"
+            alt={profilePic}
+          />
         </div>
+        <h5
+          style={{ color: '#fff', justifyContent: 'center', display: 'flex' }}
+        >
+          {name}
+        </h5>
+        <h5
+          style={{ color: '#fff', justifyContent: 'center', display: 'flex' }}
+        >
+          Co-founder
+        </h5>
       </div>
-    </Fade>
+    </div>
   );
 };
 
